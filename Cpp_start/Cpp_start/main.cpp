62e00fd2a2689dba91c9dfa0e604e55d62e21d55
pjp <panjianpeng1981@gmail.com>
2022-01-30 17:07:22 +0800

    using namespace std

--- Cpp_start/Cpp_start/main.cpp
+++ Cpp_start/Cpp_start/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cout << "Hello, World!\n";
     return 0;
 }
