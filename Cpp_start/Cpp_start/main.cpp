6714997fed74cdab90b3c4110bfd5749cec5c7c2
pjp <panjianpeng1981@gmail.com>
2022-01-30 19:20:00 +0800

    unlimited [(-10) - (10)] x [(-10) - (10)]

--- Cpp_start/Cpp_start/main.cpp
+++ Cpp_start/Cpp_start/main.cpp
@@ -5,21 +5,30 @@ using namespace std;
 int main() {
     
     const int boardSize = 21;
-    int temp;
-    cin >> temp;
-    int y;
-    cin >> y;
-    string a;
-    cin >> a;
+    int n;
+    cin >> n;
     string board[boardSize][boardSize]{};
     
-    int x = boardSize - temp - 1;
     for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) {
-            board[i][j] = ".";            
+            board[i][j] = ".";
         }
     }
-    board[x][y] = a;
+
+    //board[10][10] = "x";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        int y;
+        cin >> y;
+        string a;
+        cin >> a;
+        
+        x = x + 10;
+        y = boardSize - (y + 10) - 1;
+        board[y][x] = a;
+    }
+    
     for (int i = 0; i < boardSize; i++) {
         for(int j = 0; j < boardSize; j++) {
             cout << board[i][j];
