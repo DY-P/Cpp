0ea84ba619e72c8472ebb000c2b6a4fa6a2059ad
pjp <panjianpeng1981@gmail.com>
2022-01-30 18:25:52 +0800

    update [0-20] x [0-20] board

--- Cpp_start/Cpp_start/main.cpp
+++ Cpp_start/Cpp_start/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     
     const int boardSize = 21;
+    int temp;
+    cin >> temp;
+    int y;
+    cin >> y;
+    string a;
+    cin >> a;
+    string board[boardSize][boardSize]{};
     
+    int x = boardSize - temp - 1;
     for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) {
-            cout << ".";
+            board[i][j] = ".";            
+        }
+    }
+    board[x][y] = a;
+    for (int i = 0; i < boardSize; i++) {
+        for(int j = 0; j < boardSize; j++) {
+            cout << board[i][j];
         }
         cout << endl;
     }
