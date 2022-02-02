2e1826141d8a29a620e8f4fcbade3ec3c228e609
pdy <alphatauri65lightyears@gmail.com>
2022-02-02 21:35:37 +0800

    . display min max board

--- Cpp_start/Cpp_start/main.cpp
+++ Cpp_start/Cpp_start/main.cpp
@@ -8,7 +8,10 @@ int main() {
     int n;
     cin >> n;
     char board[boardSize][boardSize];
-    
+    int xmin = 20;
+    int xmax = 0;
+    int ymin = 20;
+    int ymax = 0;
     for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) {
             board[i][j] = '.';
@@ -17,9 +20,9 @@ int main() {
 
     //board[10][10] = "x";
     for (int i = 0; i < n; i++) {
-        int x;
+        int x; //col
         cin >> x;
-        int y;
+        int y; //row
         cin >> y;
         char a;
         cin >> a;
@@ -27,10 +30,34 @@ int main() {
         x = x + 10;
         y = boardSize - (y + 10) - 1;
         board[y][x] = a;
+        
+        if (x < ymin) {
+            ymin = x;
+        }
+        if (x > ymax) {
+            ymax = x;
+        }
+        
+        if (y < xmin) {
+            xmin = y;
+        }
+        if (y > xmax) {
+            xmax = y;
+        }
+        
+        //cout << xmin << endl << xmax << endl << ymin << endl << ymax << endl;
+        
     }
-    
-    for (int i = 0; i < boardSize; i++) {
-        for(int j = 0; j < boardSize; j++) {
+    for (int i = 0; i < (20 + 1); i++) {
+        for (int j = 0; j < (20 + 1); j++) {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+    //cout << xmin << endl << xmax << endl << ymin << endl << ymax << endl;
+    cout << endl;
+    for (int i = xmin; i < (xmax + 1); i++) {
+        for (int j = ymin; j < (ymax + 1); j++) {
             cout << board[i][j];
         }
         cout << endl;
